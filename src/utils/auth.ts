f2f53626ee89c05ca875271191c5ff2738dc6c81
[아이엔소프트] 황호세 <hshwang@in-soft.co.kr>
2022-12-05 11:04:59 +0900

    텍스트 추가

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,4 +1,7 @@
 const TokenKey = 'Admin-Token'
+//세션스토리지에 토큰을 저장하는 방식으로 작성해봤습니다.
+//하지만 보안상에 문제가 있겠죠 XSS공격에 취약하다는 특징이 있기 때문이죠.
+//따라서 yarn add react-cookie를 통해 리액트 쿠기를 받으시고 리액트 쿠키로 토큰을 관리하는 방식을 써볼까 합니다.
 
 export function getToken() {
   return sessionStorage.getItem(TokenKey)
